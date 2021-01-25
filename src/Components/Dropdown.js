ed2338bb5b449dcbf1bb72ba39d26e2730ad090d
Arkadiusz Milewski <asmilewski@gmail.com>
2021-01-25 19:33:03 +0100

    Selecting an option in dropdown manu works properly(without closing it when clic outside of dropdown menu)

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
   const renderedOptions = options.map((option) => {
+    if (selected.value === option.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={() => onSelectedChange(option)} //When click option.label => onSelectedChange =>(App.js) onSelectedChange cause setSelected
+        //so we set up selected( we select the one of the object from options array) => we pass the selected object to
+        //Dropdown.js => In Dropdown.js we can rerender a label of selected object(selected.lebel).
       >
         {option.label}
       </div>
@@ -17,10 +24,17 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label"> Select a Color</label>
-        <div className=" ui selection dropdown visible active">
+        <div
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
