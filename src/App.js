53cd67f632e1417a62ef3599e450a78859c7d61b
Arkadiusz Milewski <asmilewski@gmail.com>
2021-01-29 10:59:01 +0100

    the navigation header works properly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Accordion from "./Components/Accordion";
 import Search from "./Components/Search";
 import Dropdown from "./Components/Dropdown";
 import Translate from "./Components/Translate";
+import Route from "./Components/Route";
+import Header from "./Components/Header";
 //  eslint-disable-next-line
 const items = [
   {
@@ -36,9 +38,27 @@ const options = [
 ];
 // eslint-disable-next-line
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
